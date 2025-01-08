d5ec1b19e4eb52a5304211b3a65d7fc9445459cf
KhaliddAli <khalidali967@gmail.com>
2025-01-08 17:32:17 +0530

    Final code for ATM

--- ATM/ATM.java
+++ ATM/ATM.java
@@ -2,10 +2,20 @@ package ATM;
 import java.util.*;
 public class ATM {
     static AccountDB accountDB = new AccountDB();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
 
+        while(true){
+            int response = signIn();
+            if(response==0) break;
+        }
+
+    }
+
+    public static int signIn(){
+        int res;
         System.out.println("If you are already registered enter 1 else enter 0");
         int response = sc.nextInt();
 
@@ -20,7 +30,8 @@ public class ATM {
 
             if(accountDB.getAccount(ac)!=null){
                 Account account = accountDB.getAccount(ac);
-                chooseOperation(account);
+                res = chooseOperation(account);
+                return res;
             }
             else{
                 System.out.println("Please enter a valid A/C number!");
@@ -28,11 +39,15 @@ public class ATM {
 
         }
 
+        System.out.println("To exit enter 0 else enter 1");
+        res = sc.nextInt();
+        return res;
+
     }
 
     public static void createAccount(long balance){
         Random random = new Random();
-        long ac = random.nextLong()+1;
+        long ac = Math.abs(random.nextLong()+1);
 
         while(accountDB.getAccount(ac)!=null){
             ac = random.nextLong()+1;
@@ -42,47 +57,58 @@ public class ATM {
         accountDB.addAccount(newAccount);
     }
 
-    public static void chooseOperation(Account account){
+    public static int chooseOperation(Account account){
         Scanner sc = new Scanner(System.in);
         System.out.println("To check your Balance enter 1, To Withdraw money enter 2, To Deposit money enter 3: ");
         int response = sc.nextInt();
 
         if(response==1){
             System.out.println("Your Balance is: " + account.balance);
-            System.out.println("If you wish to exit enter 0 else enter 1 to use other services: ");
-            int res = sc.nextInt();
-            if(res==1){
-                chooseOperation(account);
-            }
-            else{
-                System.out.println("Thank you for choosing XYZ Bank!");
-            }
         }
         else if(response==2){
-            withdrawMoney(account);
-            System.out.println("If you wish to exit enter 0 else enter 1 to use other services: ");
-            int res = sc.nextInt();
-            if(res==1){
-                chooseOperation(account);
-            }
-            else{
-                System.out.println("Thank you for choosing XYZ Bank!");
-            }
+            System.out.println("Enter the withdraw amount: ");
+            long amount = sc.nextInt();
+            long balance = withdrawMoney(account,amount);
+
+            System.out.println("Withdraw successful! Final balance is: " + balance);
         }
         else if(response==3){
-            depositeMoney(account);
-            System.out.println("If you wish to exit enter 0 else enter 1 to use other services: ");
-            int res = sc.nextInt();
-            if(res==1){
-                chooseOperation(account);
-            }
-            else{
-                System.out.println("Thank you for choosing XYZ Bank!");
-            }
+            System.out.println("Enter the deposit amount: ");
+            long amount = sc.nextInt();
+            long balance = depositMoney(account,amount);
+            System.out.println("Deposit successful! Final balance is: " + balance);
         }
         else{
             System.out.println("Transaction failed!, enter a valid response!");
-            chooseOperation(account);
         }
+        System.out.println("If you wish to exit enter 0 else enter 1 to use other services: ");
+        int res = sc.nextInt();
+        if(res==1){
+            int nm = chooseOperation(account);
+        }
+        else{
+            System.out.println("Thank you for choosing XYZ Bank!");
+        }
+        return res;
+    }
+
+    public static long withdrawMoney(Account account, long amount){
+        long balance = account.balance;
+
+        if(amount>balance){
+            System.out.println("Insufficient Balance");
+            return 0;
+        }
+
+        balance-=amount;
+
+        return balance;
+
+    }
+
+    public static long depositMoney(Account account, long amount){
+        long balance = account.balance;
+        balance+=amount;
+        return balance;
     }
 }
